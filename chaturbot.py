a32c2948b8757f8bdd3942f72f67971ba499f553
L <l@Ls-MacBook-Air.local>
2019-06-09 18:14:14 -0700

    testing

--- chaturbot.py
+++ chaturbot.py
@@ -10,16 +10,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
-import time, re, json, sys, datetime
+import time, re, json, sys, datetime, os, difflib
 from configparser import ConfigParser
-from aux.read_chat import chatbox_to_txt
 
 ###### ARGUMENTS ######
 requirements_path = './req/'
 
 client = sys.argv[1] if len(sys.argv) > 1 else 'TEST'
 
-print(f'CLIENT IS {client}')
+print(f'CLIENT IS "{client}"')
 config = ConfigParser()
 config.read(requirements_path+'keys.config')
 username = config.get(client, 'username')
@@ -33,7 +32,7 @@ print(f'UPDATE EVERY: {update_every} SECONDS')
 
 ###### FUNCTIONS #######
 
-def startup(target_url, username, password, wait_time):
+def startup(target_url, username, password, wait_time, time_to_check):
     try:
         ### Use Headless browser
         gecko_path = requirements_path+'geckodriver'
@@ -42,10 +41,11 @@ def startup(target_url, username, password, wait_time):
         driver = webdriver.Firefox(options = options, executable_path=gecko_path)
         ## Wait 60 seconds before timeouts
         driver.implicitly_wait(wait_time)
-        print(f'MAX WAIT TIME BETWEEN OUTPUT: {wait_time}')
+        print(f'MAX WAIT TIME BETWEEN OUTPUT: {wait_time}s')
+        print(f'MAX TIME TO CHECK: {time_to_check}s')
         ### Go to target url
         driver.get(target_url)
-        print(f'FOUND {target_url}')
+        print(f'\nFOUND {target_url}')
         ## Wait 60 seconds before timeouts
         wait = WebDriverWait(driver, 60) 
         ## Click I accept over 18
@@ -61,7 +61,7 @@ def startup(target_url, username, password, wait_time):
         ## Input username
         login_input = wait.until(EC.visibility_of_element_located((By.ID, 'id_username')))
         login_input.send_keys(username)
-        print(f'LISTENER USERNAME: {username}')
+        print(f'\nLISTENER USERNAME: {username}')
         ## Input password
         print(f'...WAITING FOR INPUTING PASSWORD...')
         password_input = wait.until(EC.visibility_of_element_located((By.ID, 'id_password')))
@@ -71,76 +71,145 @@ def startup(target_url, username, password, wait_time):
         login_input.submit()
         print('SUBMITTED LOGIN CREDENTIALS')
         ## Return the enviornment
-        print('QUIT NOW OR REQUIRED TO KILL')
-        time.sleep(wait_time)
         print('FINISHED STARTUP')
+        print('\nQUIT NOW OR REQUIRED TO KILL')
+        time.sleep(wait_time)
+        print(f'\nOPENING ROOM...\n{target_url}')
         return (driver)
     except Exception as e:
-        print(f'STARTUP FAILED!\n{e}')
+        print(f'\nSTARTUP FAILED!\n{e}')
         driver.quit()
         sys.exit()
-    
+
+def create_sessionKey(driver):
+    try:
+        driver.get(target_url)
+        print('LOCATING CHAT-BOX...')
+        driver.find_element_by_class_name('chat-box')
+        print('LOCATED CHAT-BOX')
+        session = datetime.datetime.now()
+        print(f'\nCREATED SESSION: {session}')
+        return session
+    except Exception as e:
+        print(f'\nUNABLE TO LOCATE CHAT-BOX {datetime.datetime.now()}\nEXCEPTION:\n{e}')
+        return False
+
+##### TIP TRACKER ######
+
+def tips_to_csv(first, update_every, driver, csv_file_path, session):
+    try:
+        try:
+            collection = list(pd.read_csv(csv_file_path).drop('Unnamed: 0', axis=1).to_dict(orient='index').values())
+            if not first:
+                print(f'\nLOADED {csv_file_path}')
+        except:
+            print(f'\nCSV NOT FOUND: CREATING NEW CSV {csv_file_path}!')
+            collection = []
+        scrapeMe = driver.find_element_by_class_name('chat-box').text
+        scrape_chatbox(collection, scrapeMe, session)
+        pd.DataFrame(collection).to_csv(csv_file_path)
+        time.sleep(update_every)
+        #driver.find_element_by_class_name('chat-box')
+        return True
+    except Exception as e:
+        print(f"TIPS TO CSV FAILED!!!\nENDING SESSION!!!\nEXCEPTION:{e}")
+        return False
+
 def scrape_chatbox(data, scrapeMe, session):
     
     pattern = re.compile(r'Notice: (\{[^\}]*\})') #sender.js determines pattern of data sent
     date = re.compile(r'"date":(\d*)\}')
     matches = pattern.finditer(scrapeMe) #find all matches of useful data in Chat-Box
+    try:
+        print(f"CSV LAST-ENTRY: {data[-1]['date']}")
+    except:
+        print('CSV UPDATED: NO LAST ENTRY')
     for match in matches:
         Json = match.group(1)
         try:
             dt = int(date.search(Json).group(1)) 
-            if len(data): #check if dataframe is empty or existing
-                if (dt > data[-1]['date']): #if json we found is newer than last entry in df, append
+            x = len(data)
+            if x: #check if dataframe is empty or existing
+                if (dt > int(data[-1]['date'])): #if json we found is newer than last entry in df, append
                     Json = json.loads(Json)
                     Json['session'] = session #add session key to json object before appending
                     data.append(Json)
             else:
                 Json = json.loads(Json)
                 Json['session'] = session
-                data.append(Json)
+                data.append(Json)  
         except Exception as e:
-            print(f"ERROR IN SCRAPE CHATBOX FUNC... \n{e}")
+            print(f"\nERROR IN SCRAPE CHATBOX FUNC... \n{e}")
+    print(f"APPENDED {len(data) - x} ENTRIES")
 
-def ChaturBot2 (target_url, username, password, update_every, time_to_check, csv_file_path, wait_time):
-    driver = startup(target_url, username, password, wait_time)
+##### COMPETITION TRACKER #####
+
+def competition_chat(update_every, driver, target_url, session):
+    try:
+        name = model_name(target_url)
+        new_data = driver.find_element_by_class_name('chat-box').text
+        chatbox_to_txt(new_data, name+session, name)
+        print(f'UPDATED {name}/{name+session}')
+        time.sleep(update_every)
+        return True
+    except:
+        return False
+
+def chatbox_to_txt(newData, txt_path, dir_path):
+    try:
+        os.mkdir(dir_path, '0755' )
+        print(f'CREATED {dir_path}!!')
+    except:
+        print(f'FOUND {dir_path}...')
+    f = open(f"{dir_path+txt_path}", 'w')
+    f.close()
+    f = open(f"{dir_path+txt_path}", 'r')
+    oldD = f.read()
+    f.close()
+    diff = list(difflib.Differ().compare(oldD, newData))
+    appendme = ""
+    f = open(f"{dir_path+txt_path}", 'a')
+    for x in diff:
+        if(x[0] == '+'):
+            appendme = appendme+x[2]
+    print(appendme, file=f)
+    f.close()
+
+def model_name(target_url):
+    pattern = re.compile(r'(https://chaturbate.com/)([^/]*)/?')
+    match = pattern.search(target_url)
+    return match.group(2)
+
+
+##### MAIN BOT ##### 
+
+
+def ChaturBot_csv (target_url, username, password, update_every, time_to_check, csv_file_path, wait_time):
+    driver = startup(target_url, username, password, wait_time, time_to_check)
     driver.implicitly_wait(wait_time)
     while (True):
-        print(f'OPENING ROOM...\n{target_url}')
-        try : #attempts to find chat-box
-            driver.get(target_url)
-            print('LOCATING CHAT-BOX...')
-            driver.find_element_by_class_name('chat-box')
-            print('LOCATED CHAT-BOX')
-            session = datetime.datetime.now()
-            print(f'CREATED SESSION: {session}')
-            x = 0
-            while (x < time_to_check):
-                try:
-                    collection = list(pd.read_csv(csv_file_path).drop('Unnamed: 0', axis=1).to_dict(orient='index').values())
-                    if (x == 0):
-                        print(f'LOADED {csv_file_path}')
-                except:
-                    print(f'CSV NOT FOUND: CREATING NEW CSV {csv_file_path}!')
-                    collection = []
-                try:
-                    scrapeMe = driver.find_element_by_class_name('chat-box').text
-                    #START - AUXILARY FOR MILAN
-                    chatbox_to_txt(scrapeMe, 'forMilan.txt')
-                    #END
-                    scrape_chatbox(collection, scrapeMe, session)
-                    try:
-                        print(f"CSV UPDATED: {collection[-1]['date']}")
-                    except:
-                        print('CSV UPDATED: NO LAST ENTRY')
-                    pd.DataFrame(collection).to_csv(csv_file_path)
-                    time.sleep(update_every)
-                    x += update_every
-                    driver.find_element_by_class_name('chat-box')
-                except Exception as e:
-                    print(f'SESSION HAS ENDED {datetime.datetime.now()}\nEXCEPTION:\n{e}')
-                    break
-            print(f'{time_to_check} REACHED!!\nCHECKING IF MODEL IS ONLINE...')
-        except Exception as e:
-            print(f'UNABLE TO LOCATE CHAT-BOX {datetime.datetime.now()}\nEXCEPTION:\n{e}')
+            sessionKey = create_sessionKey(driver)
+            if sessionKey:
+                print(f'BEGINNING {tips_to_csv}')
+                for first in range(0, time_to_check, update_every):
+                    if not tips_to_csv(first, update_every, driver, csv_file_path, sessionKey):
+                        break
+                print(f'\n{time_to_check} REACHED!!\nCHECKING IF MODEL IS ONLINE...')
+            else:
+                print('FAILED')
+
+def ChaturBot_competition (target_url, username, password, update_every, time_to_check, csv_file_path, wait_time):
+    driver = startup(target_url, username, password, wait_time, time_to_check)
+    driver.implicitly_wait(wait_time)
+    while (True):
+            sessionKey = create_sessionKey(driver)
+            if sessionKey:
+                print(f'BEGINNING {tips_to_csv}')
+                for _ in range(0, time_to_check, update_every):
+                    if not competition_chat(update_every, driver, target_url, sessionKey):
+                        break
+                print(f'\n{time_to_check} REACHED!!\nCHECKING IF MODEL IS ONLINE...')
+            else:
+                print('FAILED')
 
 ChaturBot2(target_url, username, password, update_every, time_to_check, csv_file_path, 60)
