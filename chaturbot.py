15b32e5a947635fc6242de68df3dbf572f35d2f6
Hy8k9z00hgav <hy8k9z00hgav@Hy8k9z00hgavs-MacBook-Air.local>
2019-07-03 16:38:48 -0400

    cleaned up and functioning

--- chaturbot.py
+++ chaturbot.py
@@ -1,79 +1,56 @@
-##
-# ssh into server
-# run script, pipe to output file
-# crtl A + D
-##
-
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import pandas as pd
-import time, re, json, sys, datetime, os, difflib
+import time, re, json, sys, datetime, os
 from configparser import ConfigParser
-
-###### ARGUMENTS ######
-requirements_path = './req/'
-
-client = sys.argv[1] if len(sys.argv) > 1 else 'TEST'
-
-print(f'CLIENT IS "{client}"')
-config = ConfigParser()
-config.read(requirements_path+'keys.config')
-username = config.get(client, 'username')
-password = config.get(client, 'password')
-target_url = config.get(client, 'target_url')
-csv_file_path = config.get(client, 'csv_file_path')
-
-time_to_check = int(config.get('SETTINGS', 'time_to_check'))
-update_every = int(config.get('SETTINGS', 'update_every'))
-print(f'UPDATE EVERY: {update_every} SECONDS')
 
 ###### FUNCTIONS #######
 
-def startup(target_url, username, password, wait_time, time_to_check):
+def startup(requirements_path, target_url, login, settings):
     try:
-        ### Use Headless browser
-        gecko_path = requirements_path+'geckodriver'
+        ##unpack paramenters
+        headless = settings['headless']
+        geckodriver = settings['geckodriver']
+        timeout = settings['timeout']
+        csv_update = settings['csv_update']
+        restart_after = settings['time_till_restart']
+        ### Startup geckodriver
+        gecko_path = requirements_path + geckodriver
         options = Options()
-        options.headless = True
+        options.headless = headless
         driver = webdriver.Firefox(options = options, executable_path=gecko_path)
         ## Wait 60 seconds before timeouts
-        driver.implicitly_wait(wait_time)
-        print(f'MAX WAIT TIME BETWEEN OUTPUT: {wait_time}s')
-        print(f'MAX TIME TO CHECK: {time_to_check}s')
+        driver.implicitly_wait(timeout)
+        print(f'MAX WAIT TIME BETWEEN STATUS UPDATES: {timeout}s')
+        print(f'UPDATE CSV EVERY: {csv_update} SECONDS')
+        print(f'TIME UNTIL RESTART: {restart_after}s')
         ### Go to target url
         driver.get(target_url)
         print(f'\nFOUND {target_url}')
-        ## Wait 60 seconds before timeouts
-        wait = WebDriverWait(driver, 60) 
         ## Click I accept over 18
-        accept_18 = wait.until(EC.element_to_be_clickable((By.ID, 'close_entrance_terms')))
-        accept_18.click()
+        driver.find_element_by_id('close_entrance_terms').click()
         print(f'ACCEPTED 18YR OLD CHECK')
         print(f'...WAITING FOR LOGIN PAGE...')
         ## Redirect to Login Page
-        login_page = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'login-link')))
-        login_page.click()
+        driver.find_element_by_class_name('login-link').click()
         print(f'GOING TO LOGIN PAGE')
         print(f'...WAITING FOR INPUTING USERNAME...')
         ## Input username
-        login_input = wait.until(EC.visibility_of_element_located((By.ID, 'id_username')))
-        login_input.send_keys(username)
+        username = login['username']
+        driver.find_element_by_id('id_username').send_keys(username)
         print(f'\nLISTENER USERNAME: {username}')
         ## Input password
         print(f'...WAITING FOR INPUTING PASSWORD...')
-        password_input = wait.until(EC.visibility_of_element_located((By.ID, 'id_password')))
-        password_input.send_keys(password)
+        password_input = driver.find_element_by_id('id_password')
+        password_input.send_keys(login['password'])
         ## Submit form
         print(f'...WAITING TO SUBMIT CREDENTIALS...')
-        login_input.submit()
+        password_input.submit()
         print('SUBMITTED LOGIN CREDENTIALS')
         ## Return the enviornment
         print('FINISHED STARTUP')
         print('\nQUIT NOW OR REQUIRED TO KILL')
-        time.sleep(wait_time)
+        time.sleep(timeout)
         print(f'\nOPENING ROOM...\n{target_url}')
         return (driver)
     except Exception as e:
@@ -94,9 +71,7 @@ def create_sessionKey(driver):
         print(f'\nUNABLE TO LOCATE CHAT-BOX {datetime.datetime.now()}\nEXCEPTION:\n{e}')
         return False
 
-##### TIP TRACKER ######
-
-def tips_to_csv(first, update_every, driver, csv_file_path, session):
+def tips_to_csv(first, csv_update, driver, csv_file_path, session):
     try:
         try:
             collection = list(pd.read_csv(csv_file_path).drop('Unnamed: 0', axis=1).to_dict(orient='index').values())
@@ -108,8 +83,7 @@ def tips_to_csv(first, update_every, driver, csv_file_path, session):
         scrapeMe = driver.find_element_by_class_name('chat-box').text
         scrape_chatbox(collection, scrapeMe, session)
         pd.DataFrame(collection).to_csv(csv_file_path)
-        time.sleep(update_every)
-        #driver.find_element_by_class_name('chat-box')
+        time.sleep(csv_update)
         return True
     except Exception as e:
         print(f"TIPS TO CSV FAILED!!!\nENDING SESSION!!!\nEXCEPTION:{e}")
@@ -120,15 +94,11 @@ def scrape_chatbox(data, scrapeMe, session):
     pattern = re.compile(r'Notice: (\{[^\}]*\})') #sender.js determines pattern of data sent
     date = re.compile(r'"date":(\d*)\}')
     matches = pattern.finditer(scrapeMe) #find all matches of useful data in Chat-Box
-    try:
-        print(f"CSV LAST-ENTRY: {data[-1]['date']}")
-    except:
-        print('CSV UPDATED: NO LAST ENTRY')
+    x = len(data)
     for match in matches:
         Json = match.group(1)
         try:
             dt = int(date.search(Json).group(1)) 
-            x = len(data)
             if x: #check if dataframe is empty or existing
                 if (dt > int(data[-1]['date'])): #if json we found is newer than last entry in df, append
                     Json = json.loads(Json)
@@ -141,75 +111,63 @@ def scrape_chatbox(data, scrapeMe, session):
         except Exception as e:
             print(f"\nERROR IN SCRAPE CHATBOX FUNC... \n{e}")
     print(f"APPENDED {len(data) - x} ENTRIES")
-
-##### COMPETITION TRACKER #####
-
-def competition_chat(update_every, driver, target_url, session):
     try:
-        name = model_name(target_url)
-        new_data = driver.find_element_by_class_name('chat-box').text
-        chatbox_to_txt(new_data, name+session, name)
-        print(f'UPDATED {name}/{name+session}')
-        time.sleep(update_every)
-        return True
+        print(f"CSV LAST-ENTRY: {data[-1]['date']}")
     except:
-        return False
+        print('CSV UPDATED: NO LAST ENTRY')
 
-def chatbox_to_txt(newData, txt_path, dir_path):
-    try:
-        os.mkdir(dir_path, '0755' )
-        print(f'CREATED {dir_path}!!')
-    except:
-        print(f'FOUND {dir_path}...')
-    f = open(f"{dir_path+txt_path}", 'w')
-    f.close()
-    f = open(f"{dir_path+txt_path}", 'r')
-    oldD = f.read()
-    f.close()
-    diff = list(difflib.Differ().compare(oldD, newData))
-    appendme = ""
-    f = open(f"{dir_path+txt_path}", 'a')
-    for x in diff:
-        if(x[0] == '+'):
-            appendme = appendme+x[2]
-    print(appendme, file=f)
-    f.close()
+def ChaturBot_csv (requirements_path, target_url, login, settings, csv_file_path):
+    csv_update = settings['csv_update']
 
-def model_name(target_url):
-    pattern = re.compile(r'(https://chaturbate.com/)([^/]*)/?')
-    match = pattern.search(target_url)
-    return match.group(2)
-
-
-##### MAIN BOT ##### 
-
-
-def ChaturBot_csv (target_url, username, password, update_every, time_to_check, csv_file_path, wait_time):
-    driver = startup(target_url, username, password, wait_time, time_to_check)
-    driver.implicitly_wait(wait_time)
+    driver = startup(requirements_path, target_url, login, settings)
     while (True):
             sessionKey = create_sessionKey(driver)
             if sessionKey:
+                # begin tips_to_csv function
                 print(f'BEGINNING {tips_to_csv}')
-                for first in range(0, time_to_check, update_every):
-                    if not tips_to_csv(first, update_every, driver, csv_file_path, sessionKey):
+                for first in range(0, settings.time_till_restart, csv_update):
+                    if not tips_to_csv(first, csv_update, driver, csv_file_path, sessionKey):
                         break
-                print(f'\n{time_to_check} REACHED!!\nCHECKING IF MODEL IS ONLINE...')
+                print(f'\nCHECKING IF MODEL IS ONLINE...')
             else:
-                print('FAILED')
+                print(f'FAILED...\nCHECKING IF MODEL ONLINE IN {csv_update}s')
+                time.sleep(csv_update)
 
-def ChaturBot_competition (target_url, username, password, update_every, time_to_check, csv_file_path, wait_time):
-    driver = startup(target_url, username, password, wait_time, time_to_check)
-    driver.implicitly_wait(wait_time)
-    while (True):
-            sessionKey = create_sessionKey(driver)
-            if sessionKey:
-                print(f'BEGINNING {tips_to_csv}')
-                for _ in range(0, time_to_check, update_every):
-                    if not competition_chat(update_every, driver, target_url, sessionKey):
-                        break
-                print(f'\n{time_to_check} REACHED!!\nCHECKING IF MODEL IS ONLINE...')
-            else:
-                print('FAILED')
+def main():
+    #path to required files
+    requirements_path = './req/'
+    
+    
+    #grab client
+    client = sys.argv[1] if len(sys.argv) > 1 else 'TEST_CB'
+    print(f'CLIENT IS "{client}"')
+    #read config file
+    try:
+        config = ConfigParser()
+        config.read(requirements_path+'keys.config')
+        #listening bot login info
+        login = {'username': config.get(client, 'username'),
+                 'password': config.get(client, 'password')
+        }
+        #target location
+        target_url = config.get(client, 'target_url')
+        #data storage
+        csv_file_dir = config.get(client, 'csv_file_dir')
+        csv_file_path = csv_file_dir + config.get(client, 'csv_file_name')
+        os.makedirs(csv_file_dir, exist_ok=True)
+        #settings
+        settings = {'headless': bool(int(config.get('SETTINGS', 'headless'))), 
+                    'geckodriver': config.get('SETTINGS', 'geckodriver_OS'), 
+                    'time_till_restart': int(config.get('SETTINGS', 'time_till_restart')), 
+                    'csv_update': int(config.get('SETTINGS', 'update_csv_every')),
+                    'timeout': int(config.get('SETTINGS', 'wait_x_then_timeout')),
+                    }
+    except Exception as e:
+        printf(f'EXCEPTION!!:{e}')
+        return -1
+    #begin
+    ChaturBot_csv(requirements_path, target_url, login, settings, csv_file_path)
+    return 0
 
-ChaturBot2(target_url, username, password, update_every, time_to_check, csv_file_path, 60)
+if __name__ == "__main__":
+    main()
